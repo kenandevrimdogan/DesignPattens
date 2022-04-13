737ebfda6b7b838ec50efd7249696e82ee1c775b
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-13 23:59:12 +0300

    Create FileCreateInvoiker

---
+++ WebApp.Command/Commands/FileCreateInvoker.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public class FileCreateInvoker
+    {
+        private ITableActionCommand _tableActionCommand;
+        private List<ITableActionCommand> tableActionCommands = new();
+
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommand = tableActionCommand;
+        }
+
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            tableActionCommands.Add(tableActionCommand);
+        }
+
+        public Task<IActionResult> CreateFile()
+        {
+            return _tableActionCommand.Execute();
+        }
+
+        public async Task<List<IActionResult>> CreateFiles()
+        {
+            List<IActionResult> list = new();
+
+            foreach (ITableActionCommand tableActionCommand in tableActionCommands)
+            {
+                list.Add(await tableActionCommand.Execute());
+            }
+
+            return await Task.FromResult(list);
+        }
+    }
+}
