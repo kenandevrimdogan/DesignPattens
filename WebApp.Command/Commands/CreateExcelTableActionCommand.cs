17dba3c7c3fee7b7cdeb6776e245dcc853725837
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-13 23:30:34 +0300

    Create Table Action Commands

---
+++ WebApp.Command/Commands/CreateExcelTableActionCommand.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public class CreateExcelTableActionCommand<T> : ITableActionCommand
+    {
+        private readonly ExcelFile<T> _excelFile;
+
+        public CreateExcelTableActionCommand(ExcelFile<T> excelFile)
+        {
+            _excelFile = excelFile;
+        }
+
+        public async Task<IActionResult> Execute()
+        {
+            var excelMemoryStream = await _excelFile.Create();
+
+            var fileContentResult = new FileContentResult(excelMemoryStream.ToArray(), _excelFile.FileType)
+            {
+                FileDownloadName = _excelFile.FileName
+            };
+
+            return await Task.FromResult(fileContentResult);
+        }
+    }
+}
---
+++ WebApp.Command/Commands/CreatePdfTableActionCommand.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApp.Command.Commands
+{
+    public class CreatePdfTableActionCommand<T> : ITableActionCommand
+    {
+        private readonly PdfFile<T> _pdfFile;
+
+        public CreatePdfTableActionCommand(PdfFile<T> pdfFile)
+        {
+            _pdfFile = pdfFile;
+        }
+
+        public async Task<IActionResult> Execute()
+        {
+            var pdfMemoryStream = await _pdfFile.Create();
+
+            var fileContentResult = new FileContentResult(pdfMemoryStream.ToArray(), _pdfFile.FileType)
+            {
+                FileDownloadName = _pdfFile.FileName
+            };
+
+            return await Task.FromResult(fileContentResult);
+        }
+    }
+}
