695a581a231f8403e97e9b377b63edfbf0df7c4a
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-14 23:23:34 +0300

    Create Discount Table

--- WebApp.Observer/Models/AppIdentityDbContext.cs
+++ WebApp.Observer/Models/AppIdentityDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebApp.Observer.Models;
 
 namespace BasePoject.Models
 {
@@ -10,5 +11,7 @@ namespace BasePoject.Models
         {
 
         }
+
+        public DbSet<Discount> Discounts { get; set; }
     }
 }
---
+++ WebApp.Observer/Models/Discount.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Observer.Models
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public int Rate { get; set; }
+    }
+}
