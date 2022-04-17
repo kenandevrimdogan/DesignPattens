8cc86e04c69ff0f1fac7e9686109781129f26269
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-17 16:52:36 +0300

    Create ZiplFileProcessHandler

---
+++ WebApp.ChainOfResponsibility/ChainOfResponsibility/ExcelProcessHandler.cs
@@ -0,0 +1,48 @@
+﻿using ClosedXML.Excel;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace WebApp.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class ExcelProcessHandler<T> : ProcessHandler
+    {
+        private DataTable getTable(object o)
+        {
+            var table = new DataTable();
+            var type = typeof(T);
+
+            type.GetProperties().ToList().ForEach(p =>
+            {
+                table.Columns.Add(p.Name, p.PropertyType);
+            });
+
+            var list = o as List<T>;
+
+            list.ForEach(t => { 
+                var values = type.GetProperties().Select(propertyInfo => propertyInfo.GetValue(t, null)).ToList();
+
+                table.Rows.Add(values);
+            });
+
+            return table;
+        }
+
+        public override object Handle(object processHandler)
+        {
+            var wb = new XLWorkbook();
+            var ds = new DataSet();
+
+            ds.Tables.Add(getTable(processHandler));
+
+            wb.Worksheets.Add(ds);
+
+            var memoryStream = new MemoryStream();
+
+            wb.SaveAs(memoryStream);
+
+            return base.Handle(memoryStream);
+        }
+    }
+}
---
+++ WebApp.ChainOfResponsibility/ChainOfResponsibility/ZiplFileProcessHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace WebApp.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class ZiplFileProcessHandler<T> : ProcessHandler
+    {
+        public override object Handle(object processHandler)
+        {
+            var memoryStream = processHandler as MemoryStream;
+
+            memoryStream.Position = 0;
+
+            using (var packageStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(packageStream, ZipArchiveMode.Create))
+                {
+                    var zipFile = archive.CreateEntry($"{typeof(T).Name}.xlsx");
+
+                    using (var zipEntry = zipFile.Open())
+                    {
+                        memoryStream.CopyTo(zipEntry);
+                    }
+                }
+            }
+
+            return base.Handle(memoryStream);
+        }
+    }
+}
