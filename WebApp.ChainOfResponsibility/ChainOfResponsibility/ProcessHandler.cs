586d5899fac9d87718a8c089e5ebe3b675306798
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-17 16:36:43 +0300

    Create Process handler interface and base class

---
+++ WebApp.ChainOfResponsibility/ChainOfResponsibility/ProcessHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApp.ChainOfResponsibility.ChainOfResponsibility
+{
+    public abstract class ProcessHandler : IProcessHandler
+    {
+        private IProcessHandler _NextProcessHandler;
+
+        public virtual object Handle(object processHandler)
+        {
+            if(_NextProcessHandler != null)
+            {
+                return _NextProcessHandler.Handle(processHandler);
+            }
+
+            return null;
+        }
+
+        public IProcessHandler SetNext(IProcessHandler processHandler)
+        {
+            _NextProcessHandler = processHandler;
+            return _NextProcessHandler;
+        }
+    }
+}
