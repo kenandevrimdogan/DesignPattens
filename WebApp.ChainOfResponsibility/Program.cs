71b562cf4496a295008d9ca95905177b2075d163
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-17 16:29:30 +0300

    Create ChainOfResponsibility Project

---
+++ WebApp.ChainOfResponsibility/Program.cs
@@ -0,0 +1,63 @@
+using BasePoject.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.ChainOfResponsibility.Models;
+
+namespace BasePoject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+            identityDbContext.Database.Migrate();
+
+            if (!userManager.Users.Any())
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    userManager.CreateAsync(new AppUser
+                    {
+                        UserName = $"user{i}",
+                        Email = $"user{i}@outlook.com"
+                    }, "Password123*").Wait();
+                }
+
+                for (int i = 1; i <= 30; i++)
+                {
+                    identityDbContext.Products.Add(new Product
+                    {
+                       Name = $"Kalem {i}",
+                       Price = i * 100,
+                       Stock = i * 15
+                    });
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
