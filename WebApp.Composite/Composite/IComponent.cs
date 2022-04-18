b7cb9df4f3c646c5a1b7988139c63207672feebe
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-18 22:25:19 +0300

    Create IComponent and BookComponent

---
+++ WebApp.Composite/Composite/IComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.Composite.Composite
+{
+    public interface IComponent
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        int Count();
+
+        string Display();
+    }
+}
