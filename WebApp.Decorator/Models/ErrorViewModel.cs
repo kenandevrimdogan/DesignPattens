572e515620a42f0de301e0cfd34baa8e11d43ea8
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-20 21:55:52 +0300

    Create WebApp.Decorator Project

---
+++ WebApp.Decorator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BasePoject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
