6c3077981246973612cba9438bf2fff1dfd359a1
Kenan Devrim DOĞAN <devrimkenandogan@outlook.com>
2022-04-21 21:29:15 +0300

    Add adapter class

---
+++ WebApp.Adapter/Services/AdvanceImageProcessAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace WebApp.Adapter.Services
+{
+    public class AdvanceImageProcessAdapter: IImageProcess
+    {
+        private readonly IAdvanceImageProcess _advanceImageProcess;
+
+        public AdvanceImageProcessAdapter(IAdvanceImageProcess advanceImageProcess)
+        {
+            _advanceImageProcess = advanceImageProcess;
+        }
+
+        public void AddWatermark(string watermark, string fileName, Stream imageStream)
+        {
+            _advanceImageProcess.AddWatermarkImage(imageStream, watermark, $"wwwroot/watermarks/{fileName}", Color.FromArgb(128, 255, 255, 255), Color.FromArgb(120, 255, 255, 255));
+        }
+    }
+}
